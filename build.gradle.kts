392a9b99aeb6a8324504dfc15f680f138110c808
mohamedamin <funstars008@gmail.com>
2026-01-12 17:21:04 +0300

    our first coroutine

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5")
 }
 
 kotlin {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,67 @@
 package org.example
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    // a function is a sequence of instructions that takes inputs and gives us outputs
+    // a thread describes in which context the sequence of instructions should be executed
+    // when you launch the app it executes on the main thread
+
+    // Coroutines can be paused and resumed without stopping the whole program.
+
+    //Coroutines are lightweight threads —
+    // they let you run multiple small tasks without slowing down your app.
+    // They don’t block the program while waiting for long operations to finish.
+
+    // coroutines a job to execute some instructions
+    // you can execute many coroutines inside a single thread
+    // Coroutines executes inside that thread
+    // coroutines are suspendable
+
+
+    // our first coroutine
+
+    //GlobalScope lives forever
+    //It is not tied to your program lifecycle
+    //Can cause memory leaks
+
+    // delay will only pause the current coroutine
+    // and will not block the whole thread like
+    // thread.sleep does
+
+
+    val job: Job = GlobalScope.launch {
+
+
+       delay(3000)
+        println("coroutine says hello from thread: ${Thread.currentThread().name}")
     }
+
+    println("hello from thread: ${Thread.currentThread().name}")
+
+
+    // Because main() finishes before the coroutine gets a chance to run.
+
+     // we will use
+    // runBlocking keeps the program alive
+    // The coroutine gets time to run
+    // join() waits until the coroutine finishes
+
+    runBlocking {
+        job.join()
+    }
+
+    //Key takeaway
+    //launch does NOT block the thread
+    //If main() finishes, coroutines are canceled
+
+
+
 }
